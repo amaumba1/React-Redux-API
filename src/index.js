e9233da323ababe5e67419882d70027ba61cdcd1
Ally Maumba <amaumba1@gmail.com>
2018-06-20 04:12:44 -0500

    added reducers

--- src/index.js
+++ src/index.js
@@ -2,28 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
 
-const stories = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    }, {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
 
 
-ReactDOM.render(<App stories={stories} onArchive={() => {}}/>, 
+
+ReactDOM.render(
+    <App stories={store.getState().storyState} 
+    onArchive={() => {}}
+
+    />, 
     document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import storyReducer from './storyReducer'; 
+import archiveReducer from './archiveReducer';
+
+const rootReducer = combineReducers({
+    storyState: storyReducer,
+    archiveState: archiveReducer
+})
+
+export default rootReducer; 
