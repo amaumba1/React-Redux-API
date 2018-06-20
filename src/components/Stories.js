74aba558e62d6dc9b3e3650a8e3a78d6013437ed
Ally Maumba <amaumba1@gmail.com>
2018-06-19 22:36:55 -0500

    modify components

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -3,18 +3,55 @@ import './Stories.css';
 
 import Story from './Story'
 
+const COLUMNS = {
+    title: {
+        label: 'Title',
+        width: '40%',
+    },
+    author: {
+        label: 'Author',
+        width: '30%',
+    },
+    comments: {
+        label: 'Comments',
+        width: '10%',
+    },
+   
+    points: {
+        label: 'Points',
+        width: '10%',
+    },
+    archive: {
+        width: '10%',
+    },
+}
+
+
+const StoriesHeader = ({ columns }) =>
+    <div className="stories-header">
+        {Object.keys(columns).map(key =>
+            <span
+                key={key}
+                style={{ width: columns[key].width }}
+            >
+                {columns[key].label}
+            </span>
+        )}
+    </div>
 
 const Stories = ({ stories }) => {
     return (
         <div className="stories">
-            {( stories || []).map(story =>
-                <Story 
+            <StoriesHeader columns={COLUMNS} /> 
+            {(stories || []).map(story =>
+                <Story
                     key={story.objectID}
                     story={story}
-                /> 
+                    columns={COLUMNS}
+                />
             )}
         </div>
-    );
-}
+        );
+    }
 
 export default Stories; 
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './Story.css'; 
 
-const Story = ({ story }) => {
+const Story = ({ story, columns }) => {
     const { title, url, author, num_comments, points } = story; 
 
     return (
-        <div>
-            <span>
+        <div className="story">
+            <span style={{ width: columns.title.width }}>
                 <a href={url}>{title}</a> 
             </span> 
-            <span>{author}</span>
-            <span>{num_comments}</span> 
-            <span>{points}</span>
-            <span></span> 
+            <span style={{ width: columns.author.width }}>
+                {author}
+            </span>
+            <span style={{ width: columns.comments.width }}>
+                {num_comments}
+            </span> 
+            <span style={{ width: columns.points.width }}>
+                {points}
+            </span>
         </div>
     );
 }
