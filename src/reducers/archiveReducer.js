3cec7e5d68b1bfe24c41d5bdf4d4226b30611c4f
Ally Maumba <amaumba1@gmail.com>
2018-06-21 22:12:45 -0500

    add more reducers

--- src/reducers/archiveReducer.js
+++ src/reducers/archiveReducer.js
@@ -1,7 +1,16 @@
+import { STORY_ARCHIVE } from '../constants/actionTypes'
+
 const INTIAL_STATE = []; 
+
+const applyArchiveStory = (state, action) {
+    [ ...state, action.id ]; 
+}
 
 function archiveReducer(state = INTIAL_STATE, action) {
     switch(action.type) {
+        case STORY_ARCHIVE : {
+            return applyArchiveStory(state, action); 
+        }
         default: return state;
     }
 }
